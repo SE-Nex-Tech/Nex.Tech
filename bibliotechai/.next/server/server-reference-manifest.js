c3dc05543861266941bd4700c0b42142ed170fbe
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-01-17 19:01:35 +0800

    Merge branch 'main' of https://github.com/SE-Nex-Tech/Nex.Tech

--- bibliotechai/.next/server/server-reference-manifest.js
+++ bibliotechai/.next/server/server-reference-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JdaWKzG9YBZSEwpDE5fjg8/BT97tB8qpjGp9nB/yAYQ=\"\n}"
+=======
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Brpo6C8zr8ICFsRx0mLfiL5HywnTTrKNtzG5nISDjvA=\"\n}"
+>>>>>>> Stashed changes
