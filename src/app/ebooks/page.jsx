14f9f9d13beee49215288a571e658923e0d9d83b
carlmitzchel <carlmitzchel@gmail.com>
2024-01-22 10:15:55 +0800

    Update page.jsx

--- src/app/ebooks/page.jsx
+++ src/app/ebooks/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>eBooks</div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+      repellendus nam, alias perferendis quisquam quaerat quia neque optio
+      dolores molestiae quibusdam voluptatibus pariatur. Assumenda facilis culpa
+      ipsam voluptatibus nostrum minus?
+    </div>
+  );
 };
 
 export default page;
