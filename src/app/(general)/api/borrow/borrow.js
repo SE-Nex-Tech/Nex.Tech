70793b410e8e0d77304087be570210c254241291
1123Alessandro <alessandroandyrei@gmail.com>
2024-02-15 23:54:37 +0800

    working prisma create

--- src/app/(general)/api/borrow/borrow.js
+++ src/app/(general)/api/borrow/borrow.js
@@ -11,7 +11,25 @@ export async function borrow(req) {
     }
   });
   console.log("Book to borrow: ----------------");
-  // console.log(Object.keys(books).length);
   console.log(books);
-  return 'hello world';
+
+  const aiw = await prisma.books.create({
+    data: borrow_book(req)
+  })
+
+  console.log(aiw);
+  return 'end of borrow';
+}
+
+function borrow_book(params) {
+  return {
+      book_barcode: 696969,
+      book_call_num: "hatdog",
+      book_title: "hatdog",
+      book_accession_num: 123,
+      book_author: "hev abi",
+      book_edition: "0.6",
+      book_publication_place: "sa bahay",
+      book_publisher: "OC records"
+    }
 }
