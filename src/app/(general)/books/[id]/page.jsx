1f9e60d0131b00d55c1ae0415508aa0604672d78
carlmitzchel <carlmitzchel@gmail.com>
2024-02-15 13:09:57 +0800

    styling in [id]

--- src/app/(general)/books/[id]/page.jsx
+++ src/app/(general)/books/[id]/page.jsx
@@ -2,6 +2,10 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
+import Header from "@/_components/header/Header";
+import { Center, Skeleton } from "@mantine/core";
+import styles from "./bookpage.module.scss";
+import Link from "next/link";
 
 const BookPage = () => {
   const { id } = useParams();
@@ -24,20 +28,57 @@ const BookPage = () => {
     fetchBook();
   }, [id]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   if (!book) {
     return <div>Book not found</div>;
   }
 
   return (
-    <div>
-      <h1>{book.book_title}</h1>
-      <p>{book.book_author}</p>
-      <p>{book.book_publisher}</p>
-    </div>
+    <>
+      <div>
+        <Header currentRoute={"/books"} />
+      </div>
+      <Center className={styles.center} maw="100%" m={25} h="81.5%">
+        <Link href={"/books"} className={styles.navigator}>
+          Book Information
+        </Link>
+        <div className={styles.main_container}>
+          <div className={styles.row_one}>
+            <div className={styles.main_information}>
+              <Skeleton className={styles.img_holder}></Skeleton>
+              <p>Call No.: QA76.73.J38 .H788j 2022</p>
+              <h1>{book.book_title}</h1>
+              <p>
+                <span style={{ fontWeight: "bold" }}>Author:</span>{" "}
+                {book.book_author}
+              </p>
+            </div>
+            <div className={styles.information}>
+              <div className={styles.information_text}>
+                <div className={styles.headers}>
+                  <h4>Accession No.: </h4>
+                  <h4>Edition:</h4>
+                  <h4>Publication Place:</h4>
+                  <h4>Publisher:</h4>
+                  <h4>Copyright Date:</h4>
+                  <h4>Status:</h4>
+                </div>
+                <div className={styles.contents}>
+                  <p>20152831231</p>
+                  <p>11th Edition</p>
+                  <p>New York City</p>
+                  <p>AI Publishing Inc.</p>
+                  <p>06/13/2016</p>
+                  <p>Available</p>
+                </div>
+              </div>
+              <button className={styles.btn}>Borrow</button>
+            </div>
+          </div>
+
+          <div className={styles.row_two}></div>
+        </div>
+      </Center>
+    </>
   );
 };
 
