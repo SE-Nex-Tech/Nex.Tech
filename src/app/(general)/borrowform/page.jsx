0f08c9782d1942458172c1a136945c2a801441da
Edjin Jerney <edjinjerney.payumo@gmail.com>
2024-02-17 10:42:59 +0800

    Added QR code for receipt
    Tried generating qr code using qrcode.react.

--- src/app/(general)/borrowform/page.jsx
+++ src/app/(general)/borrowform/page.jsx
@@ -3,18 +3,31 @@
 import Header from "@/_components/header/Header";
 import styles from "./borrowform.module.scss";
 import { usePathname, useRouter } from "next/navigation";
-import { TextInput, Select, Button, NumberInput, Radio, Modal } from '@mantine/core';
-import React, { useState, useCallback, useEffect } from "react";
+import { TextInput, Select, Button, NumberInput, Radio, Modal, camelToKebabCase } from '@mantine/core';
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { DateInput } from "@mantine/dates";
 import { useDisclosure } from "@mantine/hooks";
 import ReactDOM from 'react-dom';
-import {QRCodeSVG} from 'qrcode.react';
+import { QRCodeSVG } from 'qrcode.react';
 
 const BorrowForm = () => {
   const current = usePathname();
   const currentDate = new Date();
   const [value, setValue] = useState('react');
   const [opened, { open, close }] = useDisclosure(false);
+
+  const requestType = useRef("Book");
+  const requestDate = useRef("");
+  const callNum = useRef("QA76.73.J38 .H788j 2022");
+  const userType = useRef("");
+  const studentNumber = useRef("");
+  const userName = useRef("");
+  const userEmail = useRef("");
+  const userDepartment = useRef("");
+  const yearLevel = useRef("");
+  const section = useRef("");
+
+
 
   return (
 
@@ -34,7 +47,9 @@ const BorrowForm = () => {
 
                 <div className={styles.input}>
                   <label>Request Date:</label>
-                  <DateInput name="requestDate" valueFormat="DD/MM/YYYY" value={currentDate} readOnly="true" />
+                  <DateInput name="requestDate" valueFormat="DD/MM/YYYY" value={currentDate} readOnly="true"
+                    // onChange={(e) => (requestDate.current = e.target.value)}
+                  />
                 </div>
 
                 <div className={styles.input}>
@@ -48,17 +63,25 @@ const BorrowForm = () => {
 
                 <div className={styles.input}>
                   <label>Student No.:</label>
-                  <NumberInput name="studentNumber" placeholder="Enter Student Number" hideControls />
+                  <NumberInput name="studentNumber" placeholder="Enter Student Number" hideControls
+                    value={value} onChange={setValue}
+                  // onChange={(e) => (studentNumber.current = e.target.value)}
+                  />
                 </div>
 
                 <div className={styles.input}>
                   <label>Name:</label>
-                  <TextInput name="userName" placeholder="Enter Name" />
+                  <TextInput name="userName" placeholder="Enter Name"
+                    // onChange={(e) => (userName.current = e.target.value)}
+                  />
+
                 </div>
 
                 <div className={styles.input}>
                   <label>Email:</label>
-                  <TextInput name="userEmail" placeholder="Enter Email Address" />
+                  <TextInput name="userEmail" placeholder="Enter Email Address"
+                    // onChange={(e) => (userEmail.current = e.target.value)}
+                  />
                 </div>
 
                 <div className={styles.input}>
@@ -67,6 +90,7 @@ const BorrowForm = () => {
                     name="userDepartment"
                     placeholder="Select Department"
                     data={['Information Technology', 'Information Systems', 'Computer Science']}
+                    // onChange={(e) => (userDepartment.current = value)}
                   />
                 </div>
 
@@ -76,12 +100,15 @@ const BorrowForm = () => {
                     name="yearLevel"
                     placeholder="Select Year Level"
                     data={['1st Year', '2nd Year', '3rd Year', '4th Year']}
+                    // onChange={(e) => (yearLevel.current = e.target.value)}
                   />
                 </div>
 
                 <div className={styles.input}>
                   <label>Section:</label>
-                  <TextInput name="section" placeholder="Enter Section" />
+                  <TextInput name="section" placeholder="Enter Section"
+                    // onChange={(e) => (section.current = e.target.value)}
+                  />
                 </div>
 
               </div>
@@ -155,53 +182,73 @@ const BorrowForm = () => {
                     <div className={styles.infoContainer}>
                       <div className={styles.receiptInfo}>
                         <div className={styles.qrContainer}>
-
+                          <QRCodeSVG
+                            bgColor="#ebebeb"
+                            fgColor="#E8B031"
+                            value={JSON.stringify(
+                              {
+                                requestCode: "1324154123421",
+                                requestDate: requestDate.current,
+                                requestType: requestType.current,
+                                callNum: callNum.current,
+                                userType: userType.current,
+                                studentNumber: studentNumber.current,
+                                userName: userName.current,
+                                userEmail: userEmail.current,
+                                userDepartment: userDepartment.current,
+                                yearLevel: yearLevel.current,
+                                section: section.current,
+                                
+                              }
+                            )} />
                         </div>
+
                         <div className={styles.receiptLabel}>
                           <h4>Receipt No.:</h4>
                           <h4>20231204734</h4>
                         </div>
+
                       </div>
                       <div className={styles.requestDetails}>
 
                         <div className={styles.reqInfo}>
                           <h4>Call No.:</h4>
-                          <h4>QA76.73.J38 .H788j 2022</h4>
-                        </div>
-
-                        <div className={styles.reqInfo}>
-                          <h4>Accession No.:</h4>
-                          <h4>2015831231</h4>
+                          <h4>{callNum.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Request Date:</h4>
-                          <h4>12/01/23 02:30PM</h4>
+                          <h4>{requestDate.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Student No.:</h4>
-                          <h4>2021157407</h4>
+                          <h4>{studentNumber.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Name:</h4>
-                          <h4>Carl Mitzchel Padua</h4>
+                          <h4>{userName.current}</h4>
+                        </div>
+
+                        <div className={styles.reqInfo}>
+                          <h4>Email:</h4>
+                          <h4>{userEmail.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Department:</h4>
-                          <h4>Computer Science</h4>
+                          <h4>{userDepartment.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Year Level:</h4>
-                          <h4>3rd Year</h4>
+                          <h4>{yearLevel.current}</h4>
                         </div>
 
                         <div className={styles.reqInfo}>
                           <h4>Section:</h4>
-                          <h4>3CSC</h4>
+                          <h4>{section.current}</h4>
                         </div>
 
                       </div>
