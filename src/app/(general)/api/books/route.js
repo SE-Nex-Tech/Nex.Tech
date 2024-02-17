e3ef105e6fe9196a680ebff056958801cc3e41f4
1123Alessandro <alessandroandyrei@gmail.com>
2024-02-17 20:20:13 +0800

    Working data fetcher with search filtering

--- src/app/(general)/api/books/route.js
+++ src/app/(general)/api/books/route.js
@@ -4,6 +4,36 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET(request) {
+  const params = Object.fromEntries(await request.nextUrl.searchParams);
   const books = await prisma.books.findMany();
+  // console.log(params['andy'] == 'value');
+  return NextResponse.json(books);
+}
+
+export async function POST(request) {
+  const params = await request.json();
+  console.log(params);
+
+  // const books = (params['where'] == undefined) ? await prisma.books.findMany() : await prisma.books.findMany(
+  //   {
+  //     where: params['where'],
+  //   }
+  // );
+
+  let books = await prisma.books.findMany({
+    where: params['where']
+  });
+
+  if (params['search'] != undefined) {
+    const condition = {}
+    condition[params['attribute']] = {
+      contains: params['contains'],
+      mode: "insensitive"
+    }
+    books = await prisma.books.findMany({
+      where: condition
+    });
+  }
+
   return NextResponse.json(books);
 }
