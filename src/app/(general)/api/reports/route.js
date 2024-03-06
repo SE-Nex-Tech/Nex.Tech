13a0c366e1937e710fc2bd735f60bc3e7456ea85
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-06 13:25:01 +0800

    prototype for analytics fetching

--- src/app/(general)/api/reports/route.js
+++ src/app/(general)/api/reports/route.js
@@ -1,7 +1,57 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+const requests_interval = async (a, b, prisma) => {
+  return await prisma.requests.findMany({
+    where: {
+      AND: [
+        {
+          date: {
+            lte: a
+          }
+        },
+        {
+          date: {
+            gte: b
+          }
+        }
+      ]
+    }
+  })
+}
+
+const bookStatistics = async(ls, prisma) => {
+  const bookReqs = await prisma.BookRequest.findMany({
+    where: {
+      request_id: { in: ls }
+    },
+    select: {
+      request_id: true,
+      book_id: true,
+      book: true
+    }
+  });
+
+  return bookReqs;
+}
+
+const gameStatistics = async(ls, prisma) => {
+  const gameReqs = await prisma.BoardgameRequest.findMany({
+    where: {
+      request_id: { in: ls }
+    },
+    select: {
+      request_id: true,
+      boardgame_id: true,
+      boardgame: true
+    }
+  });
+
+  return gameReqs;
+}
+
 export async function POST(request) {
+  const prisma = new PrismaClient();
   const params = await request.json();
   console.log(params);
 
@@ -9,14 +59,24 @@ export async function POST(request) {
   let b = params.value2 != undefined ? new Date(params.value2) : undefined;
 
   if (!(a < b)) {
+    // todo: set analytics
     let currentDate = new Date();
     let lastMonth = new Date();
-    lastMonth.setMonth(lastMonth.getMonth() - 1)
+    lastMonth.setMonth(lastMonth.getMonth() - 1);
+
+    const result = await requests_interval(currentDate, lastMonth, prisma);
+    const requestIDs = result.map((r) => r.id);
+    const bookStats = await bookStatistics(requestIDs, prisma);
+    const gameStats = await gameStatistics(requestIDs, prisma);
 
     return NextResponse.json({
       invalid_dates: 1,
+      result
     });
   }
 
+  prisma.$disconnect()
+
   return NextResponse.json({});
 }
+
