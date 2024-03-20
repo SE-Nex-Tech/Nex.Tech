6bcb3ec3f4434b69ca29b525dc695817e7c0d44b
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-20 22:53:52 +0800

    hotfix for tableAuth component

--- src/_components/superadmin/tableAuth.jsx
+++ src/_components/superadmin/tableAuth.jsx
@@ -3,8 +3,39 @@ import { Table, Avatar, rem } from "@mantine/core";
 import MenuActions from "./menu";
 import { IconCheck, IconX } from "@tabler/icons-react";
 
-const TableAuth = () => {
-  const [accessGranted, setAccessGranted] = useState(true);
+const TableAuth = (data) => {
+  data = [
+    {
+      id: 1,
+      fn: "Carl Mitzchel",
+      mn: "",
+      ln: "Padua",
+      email: "carlmitzchel.padua.cics@ust.edu.ph",
+    },
+    {
+      id: 2,
+      fn: "Alessandro Andrei",
+      mn: "A",
+      ln: "Araza",
+      email: "alessandroandrei.araza.cics@ust.edu.ph",
+    },
+    {
+      id: 3,
+      fn: "Edjin Jerney",
+      mn: "",
+      ln: "Payumo",
+      email: "edjinjerney.payumo.cics@ust.edu.ph",
+    },
+  ];
+
+  const admin_list = data.map((r) => {
+    let x = r;
+    const [accessGrantedd, setAccessGrantedd] = useState(true);
+    x["initials"] = x["fn"][0] + x["ln"][0];
+    x["access"] = [accessGrantedd, setAccessGrantedd];
+    return x;
+  });
+
   return (
     <Table.ScrollContainer minWidth={500} mah="500px" type="native">
       <Table>
@@ -21,117 +52,45 @@ const TableAuth = () => {
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>
-          <Table.Tr>
-            <Table.Td>
-              <Avatar color="red" radius="xl">
-                CP
-              </Avatar>
-            </Table.Td>
-            <Table.Td>1</Table.Td>
-            <Table.Td>Carl Mitzchel</Table.Td>
-            <Table.Td></Table.Td>
-            <Table.Td>Padua</Table.Td>
-            <Table.Td>carlmitzchel.padua.cics@ust.edu.ph</Table.Td>
-            <Table.Td>
-              {accessGranted ? (
-                <IconCheck
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "green",
-                  }}
+          {admin_list.map((r) => (
+            <Table.Tr>
+              <Table.Td>
+                <Avatar color="red" radius="xl">
+                  {r.initials}
+                </Avatar>
+              </Table.Td>
+              <Table.Td>{r.id}</Table.Td>
+              <Table.Td>{r.fn}</Table.Td>
+              <Table.Td>{r.mn}</Table.Td>
+              <Table.Td>{r.ln}</Table.Td>
+              <Table.Td>{r.email}</Table.Td>
+              <Table.Td>
+                {r.access[0] ? (
+                  <IconCheck
+                    style={{
+                      width: rem(18),
+                      height: rem(18),
+                      color: "green",
+                    }}
+                  />
+                ) : (
+                  <IconX
+                    style={{
+                      width: rem(18),
+                      height: rem(18),
+                      color: "red",
+                    }}
+                  />
+                )}
+              </Table.Td>
+              <Table.Td>
+                <MenuActions
+                  setAccessGranted={r.access[1]}
+                  accessGrated={r.access[0]}
                 />
-              ) : (
-                <IconX
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "red",
-                  }}
-                />
-              )}
-            </Table.Td>
-            <Table.Td>
-              <MenuActions
-                setAccessGranted={setAccessGranted}
-                accessGranted={accessGranted}
-              />
-            </Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Td>
-              <Avatar color="red" radius="xl">
-                AA
-              </Avatar>
-            </Table.Td>
-            <Table.Td>2</Table.Td>
-            <Table.Td>Alessandro</Table.Td>
-            <Table.Td>Z</Table.Td>
-            <Table.Td>Araza</Table.Td>
-            <Table.Td>alessandro.araza.cics@ust.edu.ph</Table.Td>
-            <Table.Td>
-              {accessGranted ? (
-                <IconCheck
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "green",
-                  }}
-                />
-              ) : (
-                <IconX
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "red",
-                  }}
-                />
-              )}
-            </Table.Td>
-            <Table.Td>
-              <MenuActions
-                setAccessGranted={setAccessGranted}
-                accessGranted={accessGranted}
-              />
-            </Table.Td>
-          </Table.Tr>
-          <Table.Tr>
-            <Table.Td>
-              <Avatar color="red" radius="xl">
-                EP
-              </Avatar>
-            </Table.Td>
-            <Table.Td>3</Table.Td>
-            <Table.Td>Edjin Jerney</Table.Td>
-            <Table.Td>Z</Table.Td>
-            <Table.Td>Payumo</Table.Td>
-            <Table.Td>edjinjerney.payumo.cics@ust.edu.ph</Table.Td>
-            <Table.Td>
-              {accessGranted ? (
-                <IconCheck
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "green",
-                  }}
-                />
-              ) : (
-                <IconX
-                  style={{
-                    width: rem(18),
-                    height: rem(18),
-                    color: "red",
-                  }}
-                />
-              )}
-            </Table.Td>
-            <Table.Td>
-              <MenuActions
-                setAccessGranted={setAccessGranted}
-                accessGranted={accessGranted}
-              />
-            </Table.Td>
-          </Table.Tr>
+              </Table.Td>
+            </Table.Tr>
+          ))}
         </Table.Tbody>
       </Table>
     </Table.ScrollContainer>
