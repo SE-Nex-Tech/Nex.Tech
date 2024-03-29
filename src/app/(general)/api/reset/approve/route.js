e4399da7ef7767e9ccbf31d7eb61bc2c35a8b793
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-29 22:14:16 +0800

    approve and reject API for pass change

---
+++ src/app/(general)/api/reset/approve/route.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server'
+const { hash } = require('bcrypt')
+
+export async function POST(request) {
+  const prisma = new PrismaClient()
+  const params = await request.json()
+
+  const now = (new Date()).toISOString()
+
+  const pass = await prisma.changepassword.update({
+    where: {
+      id: params['id']
+    },
+    data: {
+      date_approved: now
+    }
+  })
+
+  const admin = await prisma.admin.update({
+    where: {
+      email: pass.email
+    },
+    data: {
+      password: await hash(pass.new_pass, 12)
+    }
+  })
+
+  prisma.$disconnect()
+  return NextResponse.json(admin)
+}
