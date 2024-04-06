6b36a74a78a79fbe37945face3d1ecf8ed0b7e0e
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-04-06 14:01:22 +0800

    Mobile-Tablet-Desktop Responsiveness

--- src/app/(general)/books/[id]/page.jsx
+++ src/app/(general)/books/[id]/page.jsx
@@ -32,6 +32,29 @@ const BookPage = () => {
     fetchBook();
   }, [id]);
 
+  const [showHeader, setShowHeader] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1105) {
+        setIsMobile(true);
+        setShowHeader(false);
+      } else {
+        setIsMobile(false);
+        setShowHeader(true);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   if (!book) {
     return <div>Book not found</div>;
   }
@@ -43,7 +66,11 @@ const BookPage = () => {
   return (
     <>
       <div>
-        <Header currentRoute={"/books"} />
+        {showHeader && (
+          <div>
+            <Header currentRoute={"/books"} />
+          </div>
+        )}
       </div>
       <Center className={styles.center} maw="100%" m={25} h="81.5%">
         <Navigator buttonText={"Go Back"} showIcon disableLink={false} />
