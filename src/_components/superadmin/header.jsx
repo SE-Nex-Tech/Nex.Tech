61b5011b951004c72b4a10c4de29a2c15cb738e2
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-04-09 14:01:49 +0800

    Route Protection

--- src/_components/superadmin/header.jsx
+++ src/_components/superadmin/header.jsx
@@ -4,8 +4,14 @@ import React from "react";
 import styles from "./header.module.scss";
 import { Button } from "@mantine/core";
 import { signOut } from "next-auth/react";
+import { useSession, getSession } from "next-auth/react";
+import Unauthenticated from "@/_components/authentication/unauthenticated";
 
 const Header = () => {
+  const { data: session, status } = useSession();
+  if (status === "unauthenticated") {
+    return <Unauthenticated />;
+  }
   return (
     <div className={styles.container}>
       <h1 className={styles.text}>Administrator</h1>
