529002a22777e5f868e09c41b39e50e34a113703
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-04-16 02:06:13 +0800

    Signup Validation

--- src/app/(general)/api/signup/route.js
+++ src/app/(general)/api/signup/route.js
@@ -1,35 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-const { hash } = require('bcrypt');
+const { hash } = require("bcrypt");
 
 export async function POST(request) {
   const prisma = new PrismaClient();
   const params = await request.json();
 
-  const pass = await hash(params['password'], 12);
+  const pass = await hash(params["password"], 12);
 
   const find = await prisma.admin.findUnique({
     where: {
-      email: params['email']
-    }
-  })
+      email: params["email"],
+    },
+  });
 
   if (find != null) {
     return NextResponse.json({
-      invalid: 1
-    })
+      invalid: 1,
+    });
   }
 
   const result = await prisma.admin.create({
     data: {
-      email: params['email'],
-      fn: params['fname'],
-      ln: params['lname'],
+      email: params["email"],
+      fn: params["fname"],
+      mn: params["mname"],
+      ln: params["lname"],
       password: pass,
-      type: 'admin',
-    }
-  })
+      type: "admin",
+    },
+  });
 
-  await prisma.$disconnect()
-  return NextResponse.json(result)
+  await prisma.$disconnect();
+  return NextResponse.json(result);
 }
