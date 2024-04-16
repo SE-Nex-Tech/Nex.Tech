43a73486ca232fbe82f5f56467f4e4eef251cf20
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2024-04-16 21:58:09 +0800

    [id] max-width and responsive button

--- src/app/(general)/books/[id]/page.jsx
+++ src/app/(general)/books/[id]/page.jsx
@@ -10,13 +10,13 @@ import { IconChevronLeft } from "@tabler/icons-react";
 import { format } from "date-fns";
 import Navigator from "@/_components/navigator/navigator";
 import Image from "next/image";
-import placeholderImg from "@/images/placeholder.jpg"
+import placeholderImg from "@/images/placeholder.jpg";
 
 const BookPage = () => {
   const { id } = useParams();
   const [book, setBook] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [queued, setQueued] = useState(false)
+  const [queued, setQueued] = useState(false);
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -27,14 +27,14 @@ const BookPage = () => {
         setBook(selectedBook);
         setLoading(false);
 
-        const res = await fetch('/api/queue', {
-          method: 'POST',
+        const res = await fetch("/api/queue", {
+          method: "POST",
           body: JSON.stringify({
-            id: parseInt(id)
-          })
-        })
-        const d = await res.json()
-        setQueued(d.bq.length > 0 ? true : false)
+            id: parseInt(id),
+          }),
+        });
+        const d = await res.json();
+        setQueued(d.bq.length > 0 ? true : false);
       } catch (error) {
         console.error("Error fetching book:", error);
       }
@@ -70,8 +70,6 @@ const BookPage = () => {
     return <div>Book not found</div>;
   }
 
-
-
   return (
     <>
       <div>
@@ -82,17 +80,35 @@ const BookPage = () => {
         )}
       </div>
       <Center className={styles.center} maw="100%" m={25} h="81.5%">
-        <Navigator buttonText={"Go Back"} showIcon disableLink={false} link={"books"} />
+        <Navigator
+          buttonText={"Go Back"}
+          showIcon
+          disableLink={false}
+          link={"books"}
+        />
         <div className={styles.main_container}>
           <div className={styles.row_one}>
             <div className={styles.main_information}>
-              {!book.image && (<Image
-                src={placeholderImg}
-                width={110} height={140}
-                className={styles.img_holder}
-                alt="Item No Image"
-              />)}
-              {book.image && (<div className={styles.img_container}><Image className={styles.image} src={book.image} width={110} height={140} alt="" /></div>)}
+              {!book.image && (
+                <Image
+                  src={placeholderImg}
+                  width={110}
+                  height={140}
+                  className={styles.img_holder}
+                  alt="Item No Image"
+                />
+              )}
+              {book.image && (
+                <div className={styles.img_container}>
+                  <Image
+                    className={styles.image}
+                    src={book.image}
+                    width={110}
+                    height={140}
+                    alt=""
+                  />
+                </div>
+              )}
               <p>{book.call_num}</p>
               <h1>{book.title}</h1>
               <p>
@@ -110,26 +126,28 @@ const BookPage = () => {
                   <h4>Status:</h4>
                 </div>
                 <div className={styles.contents}>
-                  {book.accession_num && (<p>{book.accession_num}</p>)}
-                  {!book.accession_num && (<p>N/A</p>)}
+                  {book.accession_num && <p>{book.accession_num}</p>}
+                  {!book.accession_num && <p>N/A</p>}
 
-                  {book.edition && ( <p>{book.edition}</p>)}
-                  {!book.edition && (<p>N/A</p>)}
+                  {book.edition && <p>{book.edition}</p>}
+                  {!book.edition && <p>N/A</p>}
 
-                 
+                  {book.publisher && <p>{book.publisher}</p>}
+                  {!book.publisher && <p>N/A</p>}
 
-                  {book.publisher && ( <p>{book.publisher}</p>)}
-                  {!book.publisher && (<p>N/A</p>)}
+                  {book.copyright_date && <p>{book.copyright_date}</p>}
+                  {!book.copyright_date && <p>N/A</p>}
 
-                  {book.copyright_date && ( <p>{book.copyright_date}</p>)}
-                  {!book.copyright_date && (<p>N/A</p>)}
-
-                  {book.status && ( <p>{book.status}</p>)}
-                  {!book.status && (<p>N/A</p>)}
+                  {book.status && <p>{book.status}</p>}
+                  {!book.status && <p>N/A</p>}
                 </div>
               </div>
               <Link href={`/borrowform/${book.id}?type=book`}>
-                <button className={styles.btn}>{queued ? "Reserve (Wait in queue)" : "Borrow"}</button>
+                <button className={styles.btn}>
+                  {book.status === "Unavailable"
+                    ? "Reserve (Wait in queue)"
+                    : "Borrow"}
+                </button>
               </Link>
             </div>
           </div>
